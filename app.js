6c3061e7147e4df1e818be0dbe010f4d6983f8e0
avillanMty01 <avillanMty01@yahoo.com>
2020-11-03 18:21:32 -0600

    initialized app.js
    also modified index.html & style.css

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-document.addEventListener('DOMContentLoaded' () =>{
- // our code goes here
+document.addEventListener('DOMContentLoaded', () => {
+  // our code goes here
+  // find our grid
+  const grid = document.querySelector('.grid')
+  // select all divs inside and turn them into an array
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  // find our span id=Score and also our button
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+  const width = 10
 
-
-
-
+  console.log(squares)
 })
